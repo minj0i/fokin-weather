a958331f0ba97c22b6664afb7a823df5aab1534b
minj0i <h_hoh@naver.com>
2020-06-26 20:11:31 +0900

    weather app

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import {Alert} from "react-native";
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "3fe05a889f68731a09c5fccf26b979b3";
 
@@ -10,29 +11,41 @@ export default class extends React.Component {
   state = {
     isLoading: true
   };
-  getWeather = async(latitude, logitude) => {
-    const { data } = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${logitude}&APPID=${API_KEY}`
-      );
-      console.log(data);
-  }
-  getLocation = async() => {
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
+  getLocation = async () => {
     try {
-      response = await Location.requestPermissionsAsync();
+      await Location.requestPermissionsAsync();
       const {
-        coords: {latitude, longitude}
+        coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({isLoading: false});
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
-  }
+  };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> :  null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fokin-weather
+# Fokin Weather App
 Learning React Native by Building a Foking Weather App
